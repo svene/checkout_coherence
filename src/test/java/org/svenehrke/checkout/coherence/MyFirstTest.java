54d7ef0a3e0b9daa01dfaa55bfa4d1950b090d9c
Sven Ehrke <sven.ehrke@canoo.com>
2011-04-10 10:19:24 +0200

    converted first example to test

--- src/test/java/org/svenehrke/checkout/coherence/MyFirstTest.java
+++ src/test/java/org/svenehrke/checkout/coherence/MyFirstTest.java
@@ -1,4 +1,21 @@
 package org.svenehrke.checkout.coherence;
 
+import com.tangosol.net.CacheFactory;
+import com.tangosol.net.NamedCache;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class MyFirstTest {
+
+	static final String NAME = "Sven Ehrke";
+	static final String KEY = "Name";
+
+	@Test
+	public void test1() {
+		NamedCache myCache = CacheFactory.getCache("mycache");
+		myCache.put(KEY, NAME);
+		assertEquals(NAME, myCache.get(KEY));
+	}
+
 }
